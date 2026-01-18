5380f06ad7aedf474437817541e1ccf5efa31f6b
Linar502Prog <linarik502@gmail.com>
2026-01-18 18:44:10 +0400

    Update Задание1.cpp

--- Задание1.cpp
+++ Задание1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-	int number{};
-	std::cout << "Enter number: "<<std::endl;
-	std::cin >> number;
-	std::cout << "Your number: " << number << std::endl;
+  int number{};
+  std::cout << "Enter number: " << std::endl;
+  std::cin >> number;
+  std::cout << "Your number: \n" << number << std::endl;
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 
 }
