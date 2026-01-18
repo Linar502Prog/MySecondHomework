432d010c04be121ef6a592dd86f9a63730c86807
Linar502Prog <linarik502@gmail.com>
2026-01-18 19:05:40 +0400

    Update Задание2.cpp

--- Задание2.cpp
+++ Задание2.cpp
@@ -6,9 +6,10 @@ int main() {
   SetConsoleOutputCP(1251);
   SetConsoleCP(1251);
   std::string word;
-  std::cout << "Введите слово: ";
+  std::cout << "Введите слово: " << std::endl;
   std::cin >> word;
-  std::cout << "Вы ввели: " << word << std::endl;
+  std::cout << "Вы ввели: \n" << word << std::endl;
 
   return EXIT_SUCCESS;
 }
+
