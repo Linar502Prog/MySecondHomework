f0ec450ab3bd665d4ec548491268fd26b69002df
Linar502Prog <linarik502@gmail.com>
2026-01-18 18:27:27 +0400

    Update Задание1.cpp

--- Задание1.cpp
+++ Задание1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 int main() {
-	double number{};//т.к пользователь может ввести любое число (дробное либо целое)
-	std::cout << "Enter number: ";
+	int number{};
+	std::cout << "Enter number: "<<std::endl;
 	std::cin >> number;
 	std::cout << "Your number: " << number << std::endl;
 
 	return EXIT_SUCCESS;
+
 }
